ad764ec78dd5b41927a1748ecf2ce3d3ecae03d5
sahin404 <sahinraj20@gmail.com>
2024-09-20 01:13:58 +0600

    order delete feature  done

--- src/pages/MyBookings/BookingsTable.jsx
+++ src/pages/MyBookings/BookingsTable.jsx
@@ -1,29 +1,37 @@
-const BookingsTable = ({ booking }) => {
-  const { date, email, img, price } = booking;
+const BookingsTable = ({ booking, handleDelete }) => {
+  const { date, email, img, price, _id } = booking;
+  
   return (
     <tr className="text-md">
       <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
+        <button onClick={() => handleDelete(_id)} className="btn btn-square">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </th>
       <td>
         <div className="flex items-center gap-3">
           <div className="avatar">
             <div className="w-32 rounded h-32 w-12">
-              <img
-                src={img}
-              />
+              <img src={img} />
             </div>
           </div>
-          <div>
-            {date}
-          </div>
+          <div>{date}</div>
         </div>
       </td>
-      <td>
-       {price}
-      </td>
+      <td>{price}</td>
       <td>{email}</td>
       <th>
         <button className="btn btn-ghost btn-xs">Update</button>
--- src/pages/MyBookings/MyBookings.jsx
+++ src/pages/MyBookings/MyBookings.jsx
@@ -1,10 +1,42 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import BookingsTable from "./BookingsTable";
+import Swal from "sweetalert2";
 
 const MyBookings = () => {
   const { user } = useContext(AuthContext);
   const [bookings, setBookings] = useState([]);
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+          method: "delete",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+              {
+                const remaining = bookings.filter((book) => book._id != id);
+                setBookings(remaining);
+              }
+            }
+          });
+      }
+    });
+  };
 
   useEffect(() => {
     fetch(`http://localhost:5000/bookings?email=${user?.email}`)
@@ -39,12 +71,15 @@ const MyBookings = () => {
                 <BookingsTable
                   key={booking._id}
                   booking={booking}
+                  handleDelete={handleDelete}
                 ></BookingsTable>
               ))}
             </tbody>
           </table>
         </div>
-      ) : ("")}
+      ) : (
+        ""
+      )}
     </div>
   );
 };
